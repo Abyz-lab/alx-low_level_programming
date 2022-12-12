df956a20663777f7bbeb35fdde3ebdc4fa67fd0a
Abyz-lab <zabyalew@gmail.com>
2022-12-12 02:10:10 -0800

    thank god

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print alph lower and upper cadse
+ * Return: Always (0) Sucess
+ */
+int main(void)
+{
+	char bo;
+
+	for (bo = 'a'; bo <= 'z'; bo++);
+	{
+		putchar(bo);
+	}
+	for (bo = 'A'; bo <= 'Z'; bo++);
+	{
+		putchar(bo);
+	}
+	putchar('\n');
+	return (0);
+}
