3f66df99e48d4098d293c645ecf89fee33df1573
Abyz-lab <zabyalew@gmail.com>
2022-12-12 03:10:48 -0800

    r u surprised or wht

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main print alpha except p and e
+ * Return: Always (0)
+*/
+int main(void)
+{
+	char exc;
+
+	for (exc = 'a'; exc = 'z'; exc++)
+	{
+		if (exc != 'e' && exc != 'p')
+			putchar(exc);
+	}
+	putchar('\n');
+	return (0);
+}
