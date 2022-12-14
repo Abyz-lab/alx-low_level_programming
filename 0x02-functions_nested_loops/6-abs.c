c7f195ef71ec2ff6077b2e1eac1cf2bccd8b2b0d
Abyz-lab <zabyalew@gmail.com>
2022-12-14 12:56:34 -0800

    france coming to world cup

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _abs - prints absoulte values of int.
+ * @n: the integer in the argument.
+ * Resturn: The absoulte value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
