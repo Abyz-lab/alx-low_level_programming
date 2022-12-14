68b6338e3271a3d10d7fb9587f2f2c15db6ec559
Abyz-lab <zabyalew@gmail.com>
2022-12-14 05:18:36 -0800

    is this right

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * *main print alpha in loer case.
+ * Return: Alwats (0)
+ */
+void print_alphabet(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		_putchar(alp);
+	_putchar('\n');
+}
