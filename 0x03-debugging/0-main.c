da4db18385c90fa279f885d2c38bc90788830551
Abyz-lab <zabyalew@gmail.com>
2022-12-16 05:40:33 -0800

    will pass the probation" "

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main -test functioor if prints postive or negative.
+ * @i: the num to be checked.
+ * Return: void
+ */
+
+ int main(void)
+ {
+	 int i;
+
+	 i = 0;
+	 positive_or_negative(i);
+
+	 return (0);
+ }
+
