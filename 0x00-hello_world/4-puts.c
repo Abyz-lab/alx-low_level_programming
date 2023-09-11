944e7861c005944dcc77b6d478f31f7d8cbb6bd8
Abz-lab <zabyalew@gmail.com>
2023-09-11 06:19:28 -0700

    covering the very basics of compilation and C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - begining of our program
+ * Description: prints "Programming is like building a multilingual puzzle"
+ * Return: int (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
